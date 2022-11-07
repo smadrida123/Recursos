44f27e9f9505e1f6212294edf4bf15cf378e50c6
smadrida123 <smadrida@unal.edu.co>
2022-11-07 14:32:05 -0500

    Ejemplos modulos y juego dado mayor

---
+++ Ej_modulos/tercero.py
@@ -0,0 +1,5 @@
+##ARCHIVO DESDE EL CUAL SE CONTROLA EJECUCION DEL ARCHIVO Modulos2.py 
+#deben estar definidos variables y funciones y no estar ejecutadas explicitamente
+import Modulos2
+print(Modulos2.data)
+Modulos2.get_population()
