86dd80d6c8a785e97e13904f066f904bfaf6e28e
smadrida123 <smadrida@unal.edu.co>
2022-11-13 06:24:54 -0500

    actualizacion clase entornos virtuales

--- Curso_comprehension,funciones_y_manejo_csv/Ejemplo_csv/graph.py
+++ Curso_comprehension,funciones_y_manejo_csv/Ejemplo_csv/graph.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 
-def generate_bar_chart(labels,values):
+def generate_bar_chart(name,labels,values):
   fig,ax=plt.subplots()
   ax.bar(labels,values,color="navy")
   plt.title("Tendencia de poblacion a traves de los años para pais seleccionado")
   plt.xlabel("Años")
   plt.ylabel("Cantidad de poblacion")
 
-  plt.show()
+  plt.savefig(f"{name}.png")
+  plt.close()
 
 def generate_pie_chart(labels,values):
   fig,ax=plt.subplots()
--- Curso_comprehension,funciones_y_manejo_csv/Ejemplo_csv/runner.py
+++ Curso_comprehension,funciones_y_manejo_csv/Ejemplo_csv/runner.py
@@ -4,7 +4,7 @@ import csv_reader as reader
 import graph
 
 def run():
-  data=reader.read_csv("/home/smadrida/Compartido/Compartido/Ejemplo_csv/world_population.csv")
+  data=reader.read_csv("world_population.csv")
   #para grafico de pie, linea para filtrar por continente
   data=list(filter(lambda item:item["Continent"]=="South America",data))
   labels2,values2=utils.population_percentage(data)
@@ -15,9 +15,9 @@ def run():
   if len(result)>0:
     #result saca lista de dict de pais seleccionado
     #country saca elemento en formato diccionario
-    country=result[0]
-    labels,values=utils.get_population(country)
-    graph.generate_bar_chart(labels,values)
+    country1=result[0]
+    labels,values=utils.get_population(country1)
+    graph.generate_bar_chart(country,labels,values)
 
 #Ejecuta como script el archivo main si se llama desde terminal pero si llamo el archivo example solo me ejecuta lo que yo llame controladamente
 if __name__=="__main__":
